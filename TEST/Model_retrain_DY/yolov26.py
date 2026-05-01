940ce46916389288b6925471a89483d6fbadaa2a
dy962021 <dong962021@163.com>
2026-05-01 12:53:07 +1000

    Delete TEST/Model_retrain_DY/yolov26.py
    moved to other branch

--- TEST/Model_retrain_DY/yolov26.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-def main():
-    model = YOLO("yolo26n.pt")
-    results = model.train(data="data.yaml", epochs=1000, imgsz=512,device=0)
-
-if __name__ == '__main__':
-    main()
